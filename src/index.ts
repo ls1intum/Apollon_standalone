6b371e4eb475ca24926b6a939a55e539748e9d47
Tobias Priesching <tobias.priesching@gmail.com>
2020-03-29 20:49:58 +0200

    initial commit apollon standalone

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import './styles.css';
+import { ApollonEditor, ApollonOptions, SVG, UMLModel } from '@ls1intum/apollon';
+
+const container = document.getElementById('apollon')!;
+let editor: ApollonEditor | null = null;
+let options: ApollonOptions = {
+  model: JSON.parse(window.localStorage.getItem('apollon')!),
+};
+
+export const onChange = (event: MouseEvent) => {
+  const { name, value } = event.target as HTMLSelectElement;
+  options = { ...options, [name]: value };
+  render();
+};
+
+export const onSwitch = (event: MouseEvent) => {
+  const { name, checked: value } = event.target as HTMLInputElement;
+  options = { ...options, [name]: value };
+  render();
+};
+
+export const save = () => {
+  if (!editor) return;
+
+  const model: UMLModel = editor.model;
+  localStorage.setItem('apollon', JSON.stringify(model));
+  options = { ...options, model };
+  return options;
+};
+
+export const clear = () => {
+  localStorage.removeItem('apollon');
+  options = { ...options, model: undefined };
+};
+
+export const draw = (mode?: 'include' | 'exclude') => {
+  if (!editor) return;
+
+  const filter: string[] = [...editor.model.interactive.elements, ...editor.model.interactive.relationships];
+
+  const { svg }: SVG = editor.exportAsSVG(mode && { [mode]: filter });
+  const svgBlob = new Blob([svg], { type: 'image/svg+xml' });
+  const svgBlobURL = URL.createObjectURL(svgBlob);
+  window.open(svgBlobURL);
+};
+
+const render = () => {
+  save();
+  if (editor) {
+    editor.destroy();
+  }
+  editor = new ApollonEditor(container, options);
+};
+render();
---
+++ webpack/webpack.common.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const appVersion = require('../package').version;
+
+const outputDir = path.resolve(__dirname, '../dist');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: outputDir,
+    filename: '[name].js',
+    library: 'apollon',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        enforce: 'pre',
+        use: ['tslint-loader', 'stylelint-custom-processor-loader'],
+      },
+      {
+        test: /\.tsx?/,
+        exclude: /\/node_modules\//,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true,
+              compilerOptions: {
+                declaration: false,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [
+    new CircularDependencyPlugin({ exclude: /node_modules/ }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html.ejs',
+      xhtml: true,
+      version: appVersion,
+    }),
+    new CopyPlugin([{ from: 'assets', to: outputDir }]),
+    new webpack.HashedModuleIdsPlugin(),
+  ],
+};
