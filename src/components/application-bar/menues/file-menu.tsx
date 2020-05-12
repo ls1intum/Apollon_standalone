4468453f1beff9fe3a027135e6b87788e52a5342
Tobias Priesching <tobias.priesching@gmail.com>
2020-05-12 14:54:02 +0200

    add id to NavDropdown

--- src/components/application-bar/menues/file-menu.tsx
+++ src/components/application-bar/menues/file-menu.tsx
@@ -99,7 +99,7 @@ class FileMenuComponent extends Component<OwnProps, State> {
   render() {
     return (
       <>
-        <NavDropdown title="File">
+        <NavDropdown id="file-menu-item" title="File">
           <NavDropdown.Item onClick={this.openNewDiagramModal}>New</NavDropdown.Item>
           <NavDropdown.Item onClick={this.saveDiagram}>Save</NavDropdown.Item>
           <NavDropdown.Item onClick={this.openLoadingModal}>Load</NavDropdown.Item>
