881b17c97adda0c5e84b439c78212b3d0079e032
Tobias Priesching <tobias.priesching@gmail.com>
2020-05-14 22:32:36 +0200

    fork ts checker plugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,10 +1,21 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+
 module.exports = merge(common, {
   mode: 'production',
   devtool: false,
   output: {
     filename: '[name].[contenthash].js',
   },
+
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      useTypescriptIncrementalApi: true,
+      memoryLimit: 4096,
+    }),
+  ],
 });
