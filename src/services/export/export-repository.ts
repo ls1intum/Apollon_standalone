de6082102fdc2a1a6a60c483f113b11aadd0ea8e
Tobias Priesching <tobias.priesching@gmail.com>
2020-07-31 17:22:27 +0200

    increment patch version number use new apollon version which fixes some bugs

--- src/services/export/export-repository.ts
+++ src/services/export/export-repository.ts
@@ -1,27 +1,27 @@
 import { ApollonEditor } from '@ls1intum/apollon';
-import {ExportActionTypes, ExportJSONAction, ExportPNGAction, ExportSVGAction} from './export-types';
-import {Diagram} from "../local-storage/local-storage-types";
+import { ExportActionTypes, ExportJSONAction, ExportPNGAction, ExportSVGAction } from './export-types';
+import { Diagram } from '../local-storage/local-storage-types';
 
 export const ExportRepository = {
   exportAsSVG: (editor: ApollonEditor, diagramTitle: string): ExportSVGAction => ({
     type: ExportActionTypes.EXPORT_SVG,
     payload: {
-      editor: editor,
-      diagramTitle: diagramTitle,
+      editor,
+      diagramTitle,
     },
   }),
   exportAsPNG: (editor: ApollonEditor, diagramTitle: string): ExportPNGAction => ({
     type: ExportActionTypes.EXPORT_PNG,
     payload: {
-      editor: editor,
-      diagramTitle: diagramTitle,
+      editor,
+      diagramTitle,
     },
   }),
   exportAsJSON: (editor: ApollonEditor, diagram: Diagram): ExportJSONAction => ({
     type: ExportActionTypes.EXPORT_JSON,
     payload: {
-      editor: editor,
-      diagram: diagram,
+      editor,
+      diagram,
     },
   }),
 };
