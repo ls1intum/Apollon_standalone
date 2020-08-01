86534002a2d41e17aa1a0d931cd6583e7985378f
Tobias Priesching <tobias.priesching@gmail.com>
2020-08-01 13:33:34 +0200

    update dependencies

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
