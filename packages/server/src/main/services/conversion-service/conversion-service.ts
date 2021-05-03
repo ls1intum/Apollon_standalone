da1004943ea866817e5577c3becfced32a7532e6
Melih Oezbeyli <melih.oezbeyli@tum.de>
2021-05-03 23:35:05 +0200

    Add conversion routes to standalone

---
+++ packages/server/src/main/services/conversion-service/conversion-service.ts
@@ -0,0 +1,29 @@
+import 'jsdom-global/register';
+import { ApollonEditor, SVG, UMLModel } from '@ls1intum/apollon';
+
+export class ConversionService {
+  convertToSvg = (model: UMLModel): SVG => {
+    //@ts-ignore
+    document.body.innerHTML = '<!doctype html><html><body><div></div></body></html>';
+
+    //JSDOM does not support getBBox so we have to mock it here
+    //@ts-ignore
+    window.SVGElement.prototype.getBBox = () => ({
+      x: 0,
+      y: 0,
+      width: 10,
+      heigth: 10,
+    });
+
+    //@ts-ignore
+    const container = document.querySelector('div');
+
+    const editor = new ApollonEditor(container, {});
+
+    editor.model = model;
+
+    const svg: SVG = editor.exportAsSVG();
+
+    return svg;
+  };
+}
