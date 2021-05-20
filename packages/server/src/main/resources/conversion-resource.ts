8b143a5c87a01881e5f07897f0322f8279190094
İsmet Melih Özbeyli <melih.oezbeyli@tum.de>
2021-05-20 22:01:11 +0200

    Add conversion routes to standalone (#12)

---
+++ packages/server/src/main/resources/conversion-resource.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+// @ts-ignore
+import pdfMake from 'pdfmake/build/pdfmake.min';
+// @ts-ignore
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { ConversionService } from '../services/conversion-service/conversion-service';
+import { UMLModel } from '@ls1intum/apollon';
+
+export class ConversionResource {
+  conversionService: ConversionService = new ConversionService();
+
+  convert = (req: Request, res: Response) => {
+    if (req.body && req.body.model) {
+      let model = req.body.model;
+      if (typeof model === 'string') {
+        model = JSON.parse(model);
+      }
+      const { svg, clip } = this.conversionService.convertToSvg(<UMLModel>(<unknown>model));
+      const { width, height } = clip;
+      pdfMake.vfs = pdfFonts.pdfMake.vfs;
+      var doc = pdfMake.createPdf({
+        content: [
+          {
+            svg,
+          },
+        ],
+        pageSize: { width, height },
+        pageMargins: 0,
+      });
+      const document = doc.getStream();
+      res.type('application/pdf');
+      document.pipe(res);
+      document.end();
+    } else {
+      res.status(400).send({ error: 'Model must be defined!' });
+    }
+  };
+
+  status = (req: Request, res: Response) => {
+    res.sendStatus(200);
+  };
+}
--- packages/server/src/main/routes.ts
+++ packages/server/src/main/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { DiagramResource } from './resources/diagram-resource';
+import { ConversionResource } from './resources/conversion-resource';
 
 // options for cors midddleware
 const options: cors.CorsOptions = {
@@ -12,12 +13,15 @@ const options: cors.CorsOptions = {
 
 export const register = (app: express.Application) => {
   const diagramResource = new DiagramResource();
+  const conversionResource = new ConversionResource();
   const router = express.Router();
   router.use(cors(options));
 
   // routes
 
   // diagrams
+  router.post('/converter/pdf', (req, res) => conversionResource.convert(req, res));
+  router.get('/converter/status', (req, res) => conversionResource.status(req, res));
   router.get('/diagrams/:token', (req, res) => diagramResource.getDiagram(req, res));
   router.post('/diagrams/publish', (req, res) => diagramResource.publishDiagram(req, res));
   router.post('/diagrams/pdf', (req, res) => diagramResource.convertSvgToPdf(req, res));
