0ab9c7b2c04ac80ddebe901c7435cf67d01c4c42
minrows <minrows95@gmail.com>
2022-04-19 15:42:16 +0200

    [MODIFIED] Minor changes (Execution of prettier)

--- packages/server/src/main/utils.ts
+++ packages/server/src/main/utils.ts
@@ -1,8 +1,8 @@
-const crypto = require("crypto");
+const crypto = require('crypto');
 const alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export function randomString(length: number) {
   let result = '';
-  for (let i = length; i > 0; --i) result += alphabet[crypto.randomInt(0,alphabet.length)];
+  for (let i = length; i > 0; --i) result += alphabet[crypto.randomInt(0, alphabet.length)];
   return result;
 }
