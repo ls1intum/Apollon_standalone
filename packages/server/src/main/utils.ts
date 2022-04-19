a99053062cd7ff0718e20a586f27566b2c6678f0
minrows <minrows95@gmail.com>
2022-04-19 15:29:10 +0200

    [MODIFIED] Replace math.random with crypto.randomInt

--- packages/server/src/main/utils.ts
+++ packages/server/src/main/utils.ts
@@ -1,7 +1,8 @@
+const crypto = require("crypto");
 const alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export function randomString(length: number) {
   let result = '';
-  for (let i = length; i > 0; --i) result += alphabet[Math.floor(Math.random() * alphabet.length)];
+  for (let i = length; i > 0; --i) result += alphabet[crypto.randomInt(0,alphabet.length)];
   return result;
 }
