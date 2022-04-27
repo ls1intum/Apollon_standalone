ea462d5b4a58dd639a8b6f6377248db8593bcc4c
Swornim Shrestha <minrows95@gmail.com>
2022-04-27 18:08:15 +0200

    Update packages/server/src/main/utils.ts
    Co-authored-by: Patrick Bassner <patrick@bassner.de>

--- packages/server/src/main/utils.ts
+++ packages/server/src/main/utils.ts
@@ -3,6 +3,6 @@ const alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 export function randomString(length: number) {
   let result = '';
-  for (let i = length; i > 0; --i) result += alphabet[crypto.randomInt(0, alphabet.length)];
+  for (let i = 0; i < length; i++) result += alphabet[crypto.randomInt(alphabet.length)];
   return result;
 }
