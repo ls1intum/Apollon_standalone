4ea854579b91df37f508999891e46c1464852525
Swornim Shrestha <minrows95@gmail.com>
2022-04-28 09:53:09 +0200

    Development: Use cryptographically secure random number generator (#26)

--- packages/server/src/main/utils.ts
+++ packages/server/src/main/utils.ts
@@ -1,7 +1,8 @@
+const crypto = require('crypto');
 const alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export function randomString(length: number) {
   let result = '';
-  for (let i = length; i > 0; --i) result += alphabet[Math.floor(Math.random() * alphabet.length)];
+  for (let i = 0; i < length; i++) result += alphabet[crypto.randomInt(alphabet.length)];
   return result;
 }
