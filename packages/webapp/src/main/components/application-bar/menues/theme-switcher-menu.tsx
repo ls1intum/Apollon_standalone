a2bcd7fc9163dcf0c5709e9e26b8f45c8f83a5dd
minrows <minrows95@gmail.com>
2022-05-11 13:03:16 +0200

    [MODIFIED] Implemented Popover while hovering on Darkmode icon

--- packages/webapp/src/main/components/application-bar/menues/theme-switcher-menu.tsx
+++ packages/webapp/src/main/components/application-bar/menues/theme-switcher-menu.tsx
@@ -3,10 +3,11 @@ import { connect } from 'react-redux';
 import { ApplicationState } from '../../store/application-state';
 import { toggleTheme } from '../../../utils/theme-switcher';
 import { localStorageThemePreference } from '../../../constant';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 type OwnProps = {};
 
-type State = { isDarkMode: boolean };
+type State = { isDarkMode: boolean, showTooltip: boolean, overrideDarkMode: boolean };
 
 type StateProps = {};
 
@@ -16,7 +17,52 @@ type Props = OwnProps & StateProps & DispatchProps;
 
 const enhance = connect<StateProps, DispatchProps, OwnProps, ApplicationState>(null, {});
 
+
+const overlayContent = (
+    <Tooltip id="tooltip-bottom">
+        <div className="popover-content" id="theme-switch-popover-content">
+            <div className="head">☾ <span>Dark Mode</span> ☾</div>
+            <div className="experimental">EXPERIMENTAL</div>
+            <div className="switch-box">
+                {/* <div><fa-icon [icon]="faSync"></fa-icon> {{ 'artemisApp.theme.sync' | artemisTranslate }}</div> */}
+                Sync with Operating System
+                {/* <input className="form-check-input" type="checkbox" /> */}
+            </div>
+
+            <div className="description">
+                <span>You can click this icon at any time to disable the dark mode if you experience problems.</span>
+            </div>
+        </div >
+    </Tooltip>
+);
+
+const themeSwitcherIcon = (
+    <svg className="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
+        <circle className="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="#AEB1B5" />
+        <g className="sun-beams" stroke="#AEB1B5">
+            <line x1="12" y1="1" x2="12" y2="3" />
+            <line x1="12" y1="21" x2="12" y2="23" />
+            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+            <line x1="1" y1="12" x2="3" y2="12" />
+            <line x1="21" y1="12" x2="23" y2="12" />
+            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+        </g>
+        <mask className="moon" id="moon-mask">
+            <rect x="0" y="0" width="100%" height="100%" fill="white" />
+            <circle cx="24" cy="10" r="6" fill="black" />
+        </mask>
+    </svg>
+);
+
+
+
 class ThemeSwitcherMenuComponent extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+    }
+
 
     componentDidMount() {
         this.updateState();
@@ -28,34 +74,27 @@ class ThemeSwitcherMenuComponent extends Component<Props, State> {
     };
 
     updateState = () => {
-        this.setState({ isDarkMode: window.localStorage.getItem(localStorageThemePreference) === 'DARK' ? true : false });
-    }
+        this.setState({ isDarkMode: window.localStorage.getItem(localStorageThemePreference) === 'DARK' ? true : false, showTooltip: false, overrideDarkMode: false });
+    };
 
 
     render() {
         return (
             <>
-                <div onClick={() => { this.updateTheme(); }}>
-                    <div className={'theme-toggle' + (this.state && this.state.isDarkMode ? ' dark' : '')} id="theme-toggle">
-                        <svg className="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
-                            <circle className="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="#AEB1B5" />
-                            <g className="sun-beams" stroke="#AEB1B5">
-                                <line x1="12" y1="1" x2="12" y2="3" />
-                                <line x1="12" y1="21" x2="12" y2="23" />
-                                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-                                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-                                <line x1="1" y1="12" x2="3" y2="12" />
-                                <line x1="21" y1="12" x2="23" y2="12" />
-                                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-                                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-                            </g>
-                            <mask className="moon" id="moon-mask">
-                                <rect x="0" y="0" width="100%" height="100%" fill="white" />
-                                <circle cx="24" cy="10" r="6" fill="black" />
-                            </mask>
-                        </svg>
+                <OverlayTrigger
+                    key="bottom"
+                    placement="bottom-start"
+                    delay={{ show: 250, hide: 200000000 }}
+                    rootCloseEvent="click"
+                    overlay={overlayContent}>
+
+                    <div onClick={() => { this.updateTheme(); }}>
+                        <div className={'theme-toggle' + (this.state && this.state.isDarkMode ? ' dark' : '')} id="theme-toggle">
+                            {themeSwitcherIcon}
+                        </div>
                     </div>
-                </div>
+
+                </OverlayTrigger>
             </>
         );
     }
