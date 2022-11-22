dd8f4e04a7e8a3e4a6e40fed4b6e7327642132da
minrows <minrows95@gmail.com>
2022-11-22 09:53:39 +0100

    [MODIFIED] Firefox close icon fix

--- packages/webapp/src/main/components/incompatability-hints/firefox-incompatibility-hint.tsx
+++ packages/webapp/src/main/components/incompatability-hints/firefox-incompatibility-hint.tsx
@@ -1,13 +1,16 @@
 import React, { ReactElement, useState } from 'react';
-import { Alert } from 'react-bootstrap';
+import { Alert, Button } from 'react-bootstrap';
 
 export const FirefoxIncompatibilityHint: React.FC = (): ReactElement | null => {
   const [show, setShow] = useState(true);
   return (
-    <Alert variant="warning" onClose={() => setShow(false)} dismissible show={show}>
+    <Alert variant="warning" onClose={() => setShow(false)} show={show}>
       {' '}
       Firefox is not fully supported - some features might not work. Please use another browser (latest Chrome or
       Safari) to make sure all features are working as expected.
+      <Button variant="warning" onClick={() => setShow(false)} type="button" className="close" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </Button>
     </Alert>
   );
 };
