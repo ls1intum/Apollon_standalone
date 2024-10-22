61b68d8c2e383645704f5392b62dab0041bc128d
egenerse <egenerse@gmail.com>
2024-10-22 16:35:10 +0200

    fix: navigate to route in load diagram

--- packages/webapp/src/main/components/modals/load-diagram-modal/load-diagram-modal.tsx
+++ packages/webapp/src/main/components/modals/load-diagram-modal/load-diagram-modal.tsx
@@ -6,8 +6,9 @@ import { LoadDiagramContent } from './load-diagram-content';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { useLocalStorage } from '../../../services/local-storage/useLocalStorage';
 import { ModalContentProps } from '../application-modal-types';
-import { createDiagram, loadDiagram } from '../../../services/diagram/diagramSlice';
+import { loadDiagram } from '../../../services/diagram/diagramSlice';
 import { ApollonEditorContext } from '../../apollon-editor-component/apollon-editor-context';
+import { useNavigate } from 'react-router-dom';
 
 export const LoadDiagramModal: React.FC<ModalContentProps> = ({ close }) => {
   const { diagram } = useAppSelector((state) => state.diagram);
@@ -18,12 +19,13 @@ export const LoadDiagramModal: React.FC<ModalContentProps> = ({ close }) => {
     const localDiagrams = LocalStorageRepository.getStoredDiagrams();
     return localDiagrams.filter((storedDiagram) => storedDiagram.id !== diagram?.id);
   };
+  const navigate = useNavigate();
 
   const onSelect = (id: string) => {
     const loadedDiagram = fromlocalStorage(id);
     if (loadedDiagram && loadedDiagram.model && editorContext?.editor) {
-      // editorContext.editor.model = loadedDiagram.model;
       dispatch(loadDiagram(loadedDiagram));
+      navigate('/', { relative: 'path' });
     }
     close();
   };
