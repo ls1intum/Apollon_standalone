b52864b454367bffdf6b39d1ead19672231858d9
Faris Demirovic <farisdemirovic16@gmail.com>
2024-11-28 13:33:49 +0100

    Attempt to fix bamboo

--- packages/webapp/src/main/components/modals/create-version-modal/create-version-modal.tsx
+++ packages/webapp/src/main/components/modals/create-version-modal/create-version-modal.tsx
@@ -10,8 +10,6 @@ import { displayError } from '../../../services/error-management/errorManagement
 import { DiagramRepository } from '../../../services/diagram/diagram-repository';
 import { setDisplayUnpublishedVersion } from '../../../services/diagram/diagramSlice';
 
-console.log('yes');
-
 export const CreateVersionModal: React.FC<ModalContentProps> = ({ close }) => {
   const dispatch = useAppDispatch();
   const diagram = useAppSelector(selectDiagram);
