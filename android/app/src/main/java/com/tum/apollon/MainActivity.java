e9a6e58320aadd5a5cd483b45aacc92f3f7f781a
Belemir Kürün <kurunbelemir@gmail.com>
2025-02-24 15:39:24 +0100

    feat: capacitor integration

---
+++ android/app/src/main/java/com/tum/apollon/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tum.apollon;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.tum.cit.ase.apollon2',
+  appName: 'Apollon Capacitor Wrapped App',
+  webDir: 'build/webapp'
+};
+
+export default config;
