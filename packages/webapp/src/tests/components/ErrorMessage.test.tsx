9b41543e1f702eeb6bb3b1f4c22b04d1190bffe6
Stephan Krusche <krusche@tum.de>
2026-01-24 21:34:42 +0100

    fix a test compile error

--- packages/webapp/src/tests/components/ErrorMessage.test.tsx
+++ packages/webapp/src/tests/components/ErrorMessage.test.tsx
@@ -1,4 +1,5 @@
 import { describe, it, expect, vi } from 'vitest';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ErrorMessage } from '../../main/components/error-handling/error-message';
